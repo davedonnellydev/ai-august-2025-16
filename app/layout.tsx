a6943d8c51a56d1226041e2327fc0f503e3a0356
Dave Donnelly <davepauldonnelly@gmail.com>
2025-08-26 14:38:32 +1000

    Project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import '@mantine/core/styles.css';
 
 import React from 'react';
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import {
+  ColorSchemeScript,
+  mantineHtmlProps,
+  MantineProvider,
+} from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Project [00]',
-  description: 'An App built for AIAugust App a Day Challenge',
+  title: 'AI-Powered Flashcard Generator',
+  description:
+    'An AI-Powered Flashcard Generator app built for AIAugust App a Day Challenge',
 };
 
 export default function RootLayout({ children }: { children: any }) {
